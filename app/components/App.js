34f98ca68ae472e66ebdfcfabe8c5eba313cbb59
Christopher Umbach <chrisumbach@gmail.com>
2016-03-30 17:54:08 -0700

    implement crud api for notes

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Notes from './Notes'
+import { connect } from 'react-redux'
+import * as noteActions from '../dux/notes'
 
+@connect(() => ({}), {
+  ...noteActions
+})
 export default class App extends React.Component {
   render() {
+    const {createNote} = this.props
     return (
       <div>
-        Hello World
+        <button onClick={createNote}>+</button>
+        <Notes />
       </div>
     )
   }
---
+++ app/dux/notes.js
@@ -0,0 +1,37 @@
+import uuid from 'node-uuid';
+
+// constants
+export const CREATE_NOTE = "CREATE_NOTE"
+export const UPDATE_NOTE = "UPDATE_NOTE"
+export const DELETE_NOTE = "DELETE_NOTE"
+
+// actions
+function action(type, payload = {}) {
+  return {type, ...payload}
+}
+
+export const createNote = () => action(CREATE_NOTE, {note: {id: uuid.v4(), task: 'New Note'}})
+export const updateNote = (id, task) => action(UPDATE_NOTE, {id, task})
+export const deleteNote = (id) => action(DELETE_NOTE, {id})
+
+// reducers
+export default (state = [], action) => {
+  switch(action.type) {
+    case CREATE_NOTE:
+      return [...state, action.note]
+
+    case UPDATE_NOTE:
+      return state.map(note => {
+        if (note.id === action.id) {
+          return {...note, task: action.task}
+        }
+        return note
+      })
+
+    case DELETE_NOTE:
+      return state.filter(note => note.id !== action.id)
+
+    default:
+      return state
+  }
+}
