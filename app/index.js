a9fa9b680d009121e0c262b5620d1d0c89a7f4cf
Christopher Umbach <chrisumbach@gmail.com>
2016-03-30 18:07:25 -0700

    finished note app

--- app/index.js
+++ app/index.js
@@ -7,13 +7,16 @@ import { createStore, compose } from 'redux'
 import rootReducer from './dux'
 import { Provider } from 'react-redux'
 import DevTools from './components/DevTools'
+import storage from './lib/storage'
 
 const createStoreWithMiddleware = compose(
   DevTools.instrument()
 )(createStore)
 
-const initialState = {}
+const APP_STORAGE = "MY_KANBAN_APP"
+const initialState = storage.get(APP_STORAGE) || {}
 const store = createStoreWithMiddleware(rootReducer, initialState)
+store.subscribe(() => storage.set(APP_STORAGE, store.getState()))
 
 
 
